a5cf4a9df5903204e4aad879e1da0321fec471c7
luiz eduardo furtini <65660052+luizefurtini@users.noreply.github.com>
2020-10-08 21:53:37 -0300

    first commit

---
+++ mult_simples.js
@@ -0,0 +1,5 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = 0;
+total = valor1*valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
